0c31f6ec5444cc99821662defc8a8f1564c2f4f0
Sabryna Chang <latte.yay@gmail.com>
2016-02-25 18:58:33 +0000

    Third party APIs

---
+++ public/js/googleMaps.js
@@ -0,0 +1,18 @@
+function initMap() {
+  // Create center marker at UCSD
+  var ucsd_ltlng = {lat:32.880, lng:-117.236};
+  var CICC = {lat: 32.885008, lng: -117.241330};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    //center: ucsd_ltlng,
+    center: {lat: 32.885008, lng: -117.241330},
+    zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+      position: CICC,
+      map: map,
+      title: 'UCSD'
+  });
+}
